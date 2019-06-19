a946ee5aa807f06df2fab31d734b68bff74386af
Yaseen Kadir <yaseenkadir@users.noreply.github.com>
2019-06-19 22:37:52 +1000

    Added project code style and removed target output

--- src/main/java/com/kadaring/snowflake/App.java
+++ src/main/java/com/kadaring/snowflake/App.java
@@ -4,7 +4,8 @@ package com.kadaring.snowflake;
  * Hello world!
  */
 public class App {
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
-    }
+
+  public static void main(String[] args) {
+    System.out.println("Hello World!");
+  }
 }
